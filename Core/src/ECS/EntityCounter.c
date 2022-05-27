7628f809d37542be27228d7508a65142559209fa
KasperJSdeVries <kasper.j.s.de.vries@gmail.com>
2022-05-27 23:38:38 +0200

    Adds an entityCounter

---
+++ Core/src/ECS/EntityCounter.c
@@ -0,0 +1,44 @@
+#include "EntityCounter.h"
+
+static bool EntityCompareFunction(void *a, void *b);
+
+EntityCounter CreateEntityCounter() {
+	EntityCounter result;
+	result.latestEntity = 1;
+	InitializeArray(sizeof(uint64_t), &result.unusedEntities);
+	return result;
+}
+
+void CleanupEntityCounter(EntityCounter entityCounter) {
+	CleanupArray(entityCounter.unusedEntities);
+}
+
+Entity AddEntity(EntityCounter entityCounter) {
+	if (Array_Length(entityCounter.unusedEntities) != 0) {
+		Entity result;
+		Array_PopFront(entityCounter.unusedEntities, &result);
+		return result;
+	}
+
+	return entityCounter.latestEntity++;
+}
+
+void RemoveEntity(EntityCounter entityCounter, Entity entity) {
+	if (entityCounter.latestEntity - 1 == entity) {
+		entityCounter.latestEntity--;
+		Entity e = entityCounter.latestEntity - 1;
+		int index;
+		while (Array_GetIndex(entityCounter.unusedEntities, EntityCompareFunction, &e, &index) == Success) {
+			int z = 0;
+			Array_InsertAtIndex(entityCounter.unusedEntities, index, &z);
+			entityCounter.latestEntity--;
+			e = entityCounter.latestEntity - 1;
+		}
+	} else {
+		Array_PushBack(entityCounter.unusedEntities, &entity);
+	}
+}
+
+bool EntityCompareFunction(void *a, void *b) {
+	return *(uint64_t *)a == *(uint64_t *)b;
+}
---
+++ Core/src/ECS/EntityCounter.h
@@ -0,0 +1,21 @@
+#ifndef ENTITYCOUNTER_H_
+#define ENTITYCOUNTER_H_
+
+#include "Array.h"
+
+// std
+#include <stdint.h>
+
+typedef uint64_t Entity;
+typedef struct {
+	uint64_t latestEntity;
+	Array unusedEntities;
+} EntityCounter;
+
+EntityCounter CreateEntityCounter();
+void CleanupEntityCounter(EntityCounter entityCounter);
+
+Entity AddEntity(EntityCounter entityCounter);
+void RemoveEntity(EntityCounter entityCounter, Entity entity);
+
+#endif //ENTITYCOUNTER_H_
